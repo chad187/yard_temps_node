35847bebbe76a342be7a6af8e69dadadc8ea5079
ff <ff@ff.com>
2026-07-22 07:18:09 -0700

    resetting for production

--- esp32-lora-node/src/config.h
+++ esp32-lora-node/src/config.h
@@ -42,7 +42,6 @@
 #endif
 
 //make sure this matches what is in frontend
-uint16_t version = 3;
 
 //#define LED_PIN 21
 const int ONE_WIRE_BUS = D0;
@@ -94,6 +93,9 @@ const uint32_t uplinkIntervalSeconds = 60UL * 60UL * 8UL;    // minutes x second
 // joinEUI - previous versions of LoRaWAN called this AppEUI
 // for development purposes you can use all zeros - see wiki for details
 #define RADIOLIB_LORAWAN_JOIN_EUI  0x0000000000000000ULL;
+
+//make sure this matches what is in frontend
+uint16_t version = 1;
 
 // the Device EUI & two keys can be generated on the TTN console 
 #ifndef RADIOLIB_LORAWAN_DEV_EUI   // Replace with your Device EUI
@@ -352,7 +354,7 @@ void processDownlink(uint8_t *data, size_t len, uint8_t port) {
 
 void setupHardware() {
   Serial.begin(115200);
-  while(!Serial) { delay(10); } //remove in production
+  //while(!Serial) { delay(10); } //remove in production
 
   sensors.begin();
   //don't use in production
